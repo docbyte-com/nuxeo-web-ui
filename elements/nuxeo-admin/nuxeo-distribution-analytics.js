ddbc40016669f6c5322a3a70bf59818a937a7e8d
Ricardo Dias <rdias@nuxeo.com>
2021-03-30 18:13:59 +0100

    WEBUI-136: conditionally disable nuxeo-distribution-analytics with user defined threshold

--- elements/nuxeo-admin/nuxeo-distribution-analytics.js
+++ elements/nuxeo-admin/nuxeo-distribution-analytics.js
@@ -64,7 +64,24 @@ Polymer({
           display: none;
         }
       }
+
+      .error {
+        border-left: 4px solid var(--nuxeo-warn-text);
+        color: var(--nuxeo-text-default);
+        padding-left: 8px;
+      }
     </style>
+
+    <nuxeo-page-provider
+      id="provider"
+      provider="nxql_search"
+      page-size="1"
+      schemas="dublincore,uid"
+      headers="[[_headers()]]"
+      skip-aggregates
+      on-error="_onError"
+    >
+    </nuxeo-page-provider>
 
     <div class="flex-layout">
       <nuxeo-card>
@@ -75,25 +92,33 @@ Polymer({
           </div>
         </div>
 
-        <nuxeo-document-distribution-chart
-          id="chart"
-          index="[[index]]"
-          path="[[path]]"
-          mode="count"
-          include-version
-          include-hidden
-          include-deleted
-        >
-        </nuxeo-document-distribution-chart>
+        <template is="dom-if" if="[[_enabled]]">
+          <nuxeo-document-distribution-chart
+            id="chart"
+            index="[[index]]"
+            mode="count"
+            include-version
+            include-hidden
+            include-deleted
+          >
+          </nuxeo-document-distribution-chart>
 
-        <div class="horizontal layout center">
-          <div>
-            <iron-icon icon="icons:track-changes"></iron-icon>
+          <div class="horizontal layout center">
+            <div>
+              <iron-icon icon="icons:track-changes"></iron-icon>
+            </div>
+            <div class="flex">
+              <paper-slider id="ratings" pin snaps max="20" max-markers="20" step="1" value="{{depth}}"></paper-slider>
+            </div>
           </div>
-          <div class="flex">
-            <paper-slider id="ratings" pin snaps max="20" max-markers="20" step="1" value="{{depth}}"></paper-slider>
-          </div>
-        </div>
+        </template>
+
+        <template is="dom-if" if="[[!_enabled]]">
+          <p class="error">
+            Distribution analytics is disabled because current query would impact quality of the service. <br />
+            Please select another path.
+          </p>
+        </template>
       </nuxeo-card>
     </div>
   `,
@@ -113,20 +138,52 @@ Polymer({
       type: Number,
       value: 7,
     },
+    enableThreshold: {
+      type: Number,
+    },
+    _enabled: {
+      type: Boolean,
+      value: false,
+    },
   },
 
   observers: ['_observeDocPath(path, depth)'],
 
+  _params() {
+    return {
+      queryParams:
+        'SELECT * FROM Document ' +
+        "WHERE ecm:mixinType != 'HiddenInNavigation' " +
+        'AND ecm:isProxy = 0 AND ' +
+        'ecm:isVersion = 0 AND ' +
+        'ecm:isTrashed = 0 AND ' +
+        `ecm:path STARTSWITH '${this.path}'`,
+    };
+  },
+
+  _headers() {
+    return {
+      'Content-Type': 'application/json',
+      accept: 'text/plain,application/json',
+      'fetch-document': 'properties',
+    };
+  },
+
   _observeDocPath() {
-    if (
-      this.path &&
-      this.path.length &&
-      this.path.endsWith('/') &&
-      (this.path !== this.$.chart.path || this.depth !== this.$.chart.maxDepth)
-    ) {
-      this.$.chart.maxDepth = this.depth;
-      this.$.chart.path = this.path;
-      this.$.chart.execute();
+    if (this.path && this.path.length && this.path.endsWith('/')) {
+      this.$.provider.params = this._params();
+      this.$.provider.fetch().then((result) => {
+        this._enabled = result.resultsCount < this.enableThreshold;
+        if (this._enabled) {
+          this.async(() => {
+            if (this.path !== this.$$('#chart').path || this.depth !== this.$$('#chart').maxDepth) {
+              this.$$('#chart').maxDepth = this.depth;
+              this.$$('#chart').path = this.path;
+              this.$$('#chart').execute();
+            }
+          });
+        }
+      });
     }
   },
 });
